37a6eba1c68274136cbb2ea39c28df8153cbee7c
karnolttl <tlkarnolt@gmail.com>
2016-07-17 23:01:26 -0400

    added text command

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 $('.list-todo').on('click', 'li', function() {
-  console.log($(this).text());
+  //console.log($(this).text());
   var el = $("<li>"+$(this).text()+"</li>");
-  $('.list-done').prepend(el);
+  $('.list-done').append(el);
   $(this).remove();
 });
 
 $('.list-done').on ('click', 'li', function() {
-  console.log($(this).text());
+  //console.log($(this).text());
   var el = $("<li>"+$(this).text()+"</li>");
   $('.list-todo').prepend(el);
   $(this).remove();
 });
 
+
 $('.entry input').keyup(function (e) {
   if (e.keyCode == 13) {
+    if ($(this).val() == ":c") {
+      $('li').each(function() {
+        $(this).remove();
+      });
+      $(this).val('');
+      return;
+    }
+    var found = false;
     var inputVal = $(this).val();
-    console.log(inputVal);
-    if (inputVal != '') {
+    var searchText = inputVal.toLowerCase().trim();
+    $('.list-todo > li').each(function(){
+      if (searchText == $(this).text().toLowerCase().trim()) {
+        console.log("yep!");
+        found = true;
+      }
+    });
+    $('.list-done > li').each(function(){
+      if (searchText == $(this).text().toLowerCase().trim()) {
+        console.log("yep");
+        $(this).remove();
+      }
+    });
+    //console.log(inputVal);
+    if (inputVal != '' && !found) {
       var el = $("<li>"+inputVal+"</li>");
       $('.list-todo').prepend(el);
       $(this).val('');
@@ -26,9 +48,9 @@ $('.entry input').keyup(function (e) {
 
 
 function addToDone(item) {
-  $('.list-done').prepend("<li>"+item+"</li>");
+  $('.list-done').append("<li>"+item+"</li>");
 }
 
 function addToDo(item2) {
-  $('.list-todo').prepend("<li>"+item2+"</li>");
+  $('.list-todo').append("<li>"+item2+"</li>");
 }
