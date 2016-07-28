d31df4971c2e78ddb7b9f885766c110e5636c92e
karnolttl <tlkarnolt@gmail.com>
2016-07-27 23:04:54 -0400

    refactor html js css

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
 var imgHtmlRemove = "<img src=\"img/remove.svg\" alt=\"\" /></li>";
 
-$('.list-todo').on('click', 'span', function() {
-  console.log($(this).text());
-  var el = $("<li><span>"+$(this).text()+"</span>"+imgHtmlRemove);
-  $('.list-done').append(el);
+$('.todo-list').on('click', 'li[class="todo"]', function() {
+  $(this).removeClass("todo");
+  $(this).addClass("done");
+});
+
+$('.todo-list').on('click', 'img', function() {
   $(this).parent().remove();
 });
 
-$('ul[class^="list-"]').on('click', 'img', function() {
-  $(this).parent().remove();
-});
-
-$('.list-done').on ('click', 'span', function() {
-  console.log($(this).text());
-var el = $("<li><span>"+$(this).text()+"</span>"+imgHtmlRemove);
-  $('.list-todo').prepend(el);
-  $(this).parent().remove();
+$('.todo-list').on ('click', 'li[class="done"]', function() {
+  $(this).removeClass("done");
+  $(this).addClass("todo");
 });
 
 
@@ -24,19 +20,21 @@ $('.entry input').keyup(function (e) {
     var found = false;
     var inputVal = $(this).val();
     var searchText = inputVal.toLowerCase().trim();
-    $('.list-todo > li').each(function(){
+    $('.todo-list > span').each(function(){
       if (searchText == $(this).text().toLowerCase().trim()) {
         found = true;
-      }
-    });
-    $('.list-done > li').each(function(){
-      if (searchText == $(this).text().toLowerCase().trim()) {
-        $(this).remove();
+        if ($(this).parent().hasClass('todo')) {
+          $(this).parent().remove();
+        }
+        else {
+          $(this).parent().removeClass("done");
+          $(this).parent().addClass("todo");
+        }
       }
     });
     if (inputVal != '' && !found) {
-      var el = $("<li><span>"+inputVal+"</span>"+imgHtmlRemove);
-      $('.list-todo').prepend(el);
+      var el = $("<li class=\"todo\"><span>"+inputVal+"</span>"+imgHtmlRemove);
+      $('.todo-list').prepend(el);
       $(this).val('');
     }
   }
