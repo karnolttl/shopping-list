d1269ad69bd0107ca3321dd3dc64a83c910ba2cf
karnolttl <tlkarnolt@gmail.com>
2016-07-27 22:20:25 -0400

    js update for icons

--- js/app.js
+++ js/app.js
@@ -1,51 +1,41 @@
 var imgHtmlRemove = "<img src=\"img/remove.svg\" alt=\"\" /></li>";
 
-//$('.list-todo').on('click', 'li', function() {
-  //console.log($(this).text());
-  //var el = $("<li>"+$(this).text()+imgHtmlRemove);
-  //$('.list-done').append(el);
-  //$(this).remove();
-//});
+$('.list-todo').on('click', 'span', function() {
+  console.log($(this).text());
+  var el = $("<li><span>"+$(this).text()+"</span>"+imgHtmlRemove);
+  $('.list-done').append(el);
+  $(this).parent().remove();
+});
 
 $('ul[class^="list-"]').on('click', 'img', function() {
   $(this).parent().remove();
 });
 
-//$('.list-done').on ('click', 'li', function() {
-  ////console.log($(this).text());
-//var el = $("<li>"+$(this).text()+imgHtmlRemove);
-  //$('.list-todo').prepend(el);
-  //$(this).remove();
-//});
+$('.list-done').on ('click', 'span', function() {
+  console.log($(this).text());
+var el = $("<li><span>"+$(this).text()+"</span>"+imgHtmlRemove);
+  $('.list-todo').prepend(el);
+  $(this).parent().remove();
+});
 
 
 $('.entry input').keyup(function (e) {
   if (e.keyCode == 13) {
-    if ($(this).val() == ":c") {
-      $('li').each(function() {
-        $(this).remove();
-      });
-      $(this).val('');
-      return;
-    }
     var found = false;
     var inputVal = $(this).val();
     var searchText = inputVal.toLowerCase().trim();
     $('.list-todo > li').each(function(){
       if (searchText == $(this).text().toLowerCase().trim()) {
-        console.log("yep!");
         found = true;
       }
     });
     $('.list-done > li').each(function(){
       if (searchText == $(this).text().toLowerCase().trim()) {
-        console.log("yep");
         $(this).remove();
       }
     });
-    //console.log(inputVal);
     if (inputVal != '' && !found) {
-      var el = $("<li>"+inputVal+imgHtmlRemove);
+      var el = $("<li><span>"+inputVal+"</span>"+imgHtmlRemove);
       $('.list-todo').prepend(el);
       $(this).val('');
     }
