47014efc08dbceea64d2eefef263e10418ec0eab
karnolttl <karn0lttl@gmail.com>
2016-08-07 00:33:01 -0400

    Added require.js

--- js/app.js
+++ js/app.js
@@ -1,80 +1,68 @@
-var imgHtmlRemove = "<img src=\"img/remove.svg\" alt=\"\" /></li>";
+define(['jquery', 'lodash'], function($,_) {
 
-function addItem() {
-    var inputVal = $('.entry input').val();
-    var searchText = inputVal.toLowerCase().trim();
-    $('li > span').each(function(){
-      if (searchText == $(this).text().toLowerCase().trim()) {
-        if ($(this).parent().hasClass('todo')) {
-          $(this).parent().animateCss('shake');
-          $('.entry input').val('');
+  imgHtmlRemove = "<img src=\"img/remove.svg\" alt=\"\" /></li>";
+
+  function addItem() {
+      var inputVal = $('.entry input').val();
+      var searchText = inputVal.toLowerCase().trim();
+      $('li > span').each(function(){
+        if (searchText == $(this).text().toLowerCase().trim()) {
+          if ($(this).parent().hasClass('todo')) {
+            $(this).parent().animateCss('shake');
+            $('.entry input').val('');
+          }
+          else {
+            $(this).parent().removeClass("done");
+            $(this).parent().addClass("todo");
+            $(this).parent().animateCss('rubberBand');
+            $('.entry input').val('');
+          }
         }
-        else {
-          $(this).parent().removeClass("done");
-          $(this).parent().addClass("todo");
-          $(this).parent().animateCss('rubberBand');
-          $('.entry input').val('');
-        }
+      });
+      inputVal = $('.entry input').val();
+      if (inputVal != '') {
+        var el = $("<li class=\"todo\"><span>"+inputVal+"</span>"+imgHtmlRemove);
+        $('.todo-list').prepend(el);
+        el.animateCss('fadeIn', false);
+        $('.entry input').val('');
       }
-    });
-    inputVal = $('.entry input').val();
-    if (inputVal != '') {
-      var el = $("<li class=\"todo\"><span>"+inputVal+"</span>"+imgHtmlRemove);
-      $('.todo-list').prepend(el);
-      el.animateCss('fadeIn', false);
-      $('.entry input').val('');
+  }
+
+  $('.todo-list').on('click', 'span', function() {
+    if ($(this).parent().hasClass("todo")) {
+      $(this).parent().removeClass("todo");
+      $(this).parent().addClass("done");
     }
-}
-
-$('.todo-list').on('click', 'span', function() {
-  if ($(this).parent().hasClass("todo")) {
-    $(this).parent().removeClass("todo");
-    $(this).parent().addClass("done");
-  }
-  else{
-    $(this).parent().removeClass("done");
-    $(this).parent().addClass("todo");
-  }
-});
-
-$('.todo-list').on('click', 'img', function() {
-  $(this).parent().animateCss('fadeOut', true);
-});
-
-$('.entry input').keyup(function (e) {
-  if (e.keyCode == 13) {
-    addItem();
-  }
-});
-
-$('.entry').on('click', 'img', addItem);
-
-$.fn.extend({
-  animateCss: function (animationName, remove) {
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-            if (remove)
-              $(this).remove();
-        });
+    else{
+      $(this).parent().removeClass("done");
+      $(this).parent().addClass("todo");
     }
+  });
+
+  $('.todo-list').on('click', 'img', function() {
+    $(this).parent().animateCss('fadeOut', true);
+  });
+
+  $('.entry input').keyup(function (e) {
+    if (e.keyCode == 13) {
+      addItem();
+    }
+  });
+
+  $('.entry').on('click', 'img', addItem);
+
+  $.fn.extend({
+    animateCss: function (animationName, remove) {
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+          $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+              $(this).removeClass('animated ' + animationName);
+              if (remove)
+                $(this).remove();
+          });
+      }
+  });
+
 });
-
-//function sortList(ul){
-  //var new_ul = ul.cloneNode(false);
-
-  //var lst = [];
-  //for(var i = ul.childNodes.length; i--;){
-    //if(ul.childNodes[i].nodeName === 'LI')
-      //lst.push(ul.childNodes[i]);
-  //}
-
-  //lst.sort();
-
-  //for(var i = 0; i < lst.length; i++)
-    //new_ul.appendChild(lst[i]);
-  //ul.parentNode.replaceChild(new_ul, ul);
-//}
 
 
 
---
+++ js/config.js
@@ -0,0 +1,14 @@
+requirejs.config({
+  baseUrl: './js',
+  deps: ['app'],
+  paths: {
+    'jquery': [
+      '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min',
+      'jquery.min'
+    ],
+    'lodash': [
+      '//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash.min',
+      'lodash.min'
+    ]
+  }
+});
